44ac469d909d52e1957a3dccc0877224c8256f67
dasanchez11 <diegosanchez@Diegos-MacBook-Pro.local>
2022-05-22 21:16:54 -0500

    heroku changes

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ app.use(cors())
 app.use(cookieParser())
 
 
-app.use((req,res,next)=> {
-    // res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000")
-    res.setHeader('Access-Control-Allow-Origin', "https://peppy-duckanoo-789c6c.netlify.app")
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next()
-})
+// app.use((req,res,next)=> {
+//     // res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000")
+//     res.setHeader('Access-Control-Allow-Origin', "https://peppy-duckanoo-789c6c.netlify.app")
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next()
+// })
 
 const stockRoutes = require('./Routes/stock.routes')
 const authRoutes = require('./Routes/auth.routes')
